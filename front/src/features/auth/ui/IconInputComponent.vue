1474a2487f6d6e330c0227832a019cf4d22bf2d0
EugeneGpil <gpilligrim@gmail.com>
2025-08-03 13:05:03 +0700

    extract IconInputComponent

---
+++ front/src/features/auth/ui/IconInputComponent.vue
@@ -0,0 +1,74 @@
+<template>
+  <div :class="`icon-input ${containerClass}`">
+    <input class="icon-input__input" :placeholder="placeholder" :type="type">
+    <div class="icon-input__icon-container">
+      <i :class="`icon-input__icon ${icon}`"/>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    containerClass: {
+      type: String,
+      default: '',
+    },
+    placeholder: {
+      type: String,
+      required: true,
+    },
+    type: {
+      type: String,
+      default: 'text',
+    },
+    icon: {
+      type: String,
+      required: true,
+    }
+  },
+}
+</script>
+
+<style scoped lang="scss">
+.icon-input {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border: solid var(--border-small) var(--color-border-secondary);
+  border-radius: var(--border-radius-secondary);
+  padding: 0;
+  overflow: hidden;
+
+  &__input {
+    flex: 1;
+    height: 100%;
+    padding: var(--gap-tiny) var(--gap-small);
+    border-radius: 0;
+    border: none;
+    border-right: solid 1px var(--color-border-secondary);
+    background-color: var(--color-background-main);
+    font-size: var(--text-usual);
+    line-height: var(--line-height-usual);
+
+    &::placeholder {
+      color: var(--color-input-placeholder);
+    }
+  }
+
+  &__icon-container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: var(--gap-tiny) var(--gap-small);
+    flex: 0.08;
+    background-color: var(--color-background-secondary);
+    align-self: stretch;
+    color: var(--color-text-secondary);
+  }
+
+  &__icon {
+    font-size: var(--gap-smaller);
+  }
+}
+</style>
--- front/src/pages/LoginPage.vue
+++ front/src/pages/LoginPage.vue
@@ -1,23 +1,22 @@
 <template>
   <div class="full-screen-form">
     <div class="full-screen-form__content">
-      <FullScreenTitleComponent
+      <TitleComponent
         title="Sign in to start your session"
         containerClass="full-screen-form__row"
       />
-      <div class="icon-input full-screen-form__row">
-        <input class="icon-input__input" placeholder="Email">
-        <div class="icon-input__icon-container">
-          <i class="icon-input__icon fa-solid fa-envelope"/>
-        </div>
-      </div>
-      <div class="icon-input full-screen-form__row">
-        <input class="icon-input__input" placeholder="Password">
-        <div class="icon-input__icon-container">
-          <i class="icon-input__icon"/>
-          <i class="icon-input__icon fa-solid fa-lock"/>
-        </div>
-      </div>
+      <IconInputComponent
+        placeholder="Email"
+        containerClass="full-screen-form__row"
+        type="email"
+        icon="fa-solid fa-envelope"
+      />
+      <IconInputComponent
+        placeholder="Password"
+        containerClass="full-screen-form__row"
+        type="password"
+        icon="fa-solid fa-lock"
+      />
       <div class="full-screen-form__small-button button">
         Sign in
       </div>
@@ -29,52 +28,11 @@
 </template>
 
 <script setup>
-import FullScreenTitleComponent from 'src/shared/ui/FullScreenTitleComponent.vue'
+import IconInputComponent from 'src/features/auth/ui/IconInputComponent.vue'
+import TitleComponent from 'src/features/auth/ui/TitleComponent.vue'
 </script>
 
 <style scoped lang="scss">
-.icon-input {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  border: solid var(--border-small) var(--color-border-secondary);
-  border-radius: var(--border-radius-secondary);
-  padding: 0;
-  overflow: hidden;
-
-  &__input {
-    flex: 1;
-    height: 100%;
-    padding: var(--gap-tiny) var(--gap-small);
-    border-radius: 0;
-    border: none;
-    border-right: solid 1px var(--color-border-secondary);
-    background-color: var(--color-background-main);
-    font-size: var(--text-usual);
-    line-height: var(--line-height-usual);
-
-    &::placeholder {
-      color: var(--color-input-placeholder);
-    }
-  }
-
-  &__icon-container {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    padding: var(--gap-tiny) var(--gap-small);
-    //padding: var(--gap-small);
-    flex: 0.08;
-    background-color: var(--color-background-secondary);
-    align-self: stretch;
-    color: var(--color-text-secondary);
-  }
-
-  &__icon {
-    font-size: var(--gap-smaller);
-  }
-}
-
 .full-screen-form {
   &__small-button {
     align-self: flex-end;
