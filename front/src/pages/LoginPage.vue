2680327b7880a8c19250b9bf3c2dce6109a60107
EugeneGpil <gpilligrim@gmail.com>
2025-08-03 13:36:12 +0700

    extract FullScreenForm.vue

--- front/src/pages/LoginPage.vue
+++ front/src/pages/LoginPage.vue
@@ -1,69 +1,33 @@
 <template>
-  <div class="full-screen-form">
-    <div class="full-screen-form__content">
-      <TitleComponent
+  <FullScreenForm>
+    <TitleComponent
         title="Sign in to start your session"
         containerClass="full-screen-form__row"
-      />
-      <IconInputComponent
+    />
+    <IconInputComponent
         placeholder="Email"
         containerClass="full-screen-form__row"
         type="email"
         icon="fa-solid fa-envelope"
-      />
-      <IconInputComponent
+    />
+    <IconInputComponent
         placeholder="Password"
         containerClass="full-screen-form__row"
         type="password"
         icon="fa-solid fa-lock"
-      />
-      <ButtonComponent
+    />
+    <ButtonComponent
         title="Sign in"
         containerClass="full-screen-form__small-button"
-      />
-      <AdditionalButton title="Register a new membership" />
-    </div>
-  </div>
+    />
+    <AdditionalButton title="Register a new membership" />
+  </FullScreenForm>
 </template>
 
 <script setup>
 import AdditionalButton from 'src/features/auth/ui/AdditionalButton.vue';
-import IconInputComponent from 'src/features/auth/ui/IconInputComponent.vue'
-import TitleComponent from 'src/features/auth/ui/TitleComponent.vue'
+import IconInputComponent from 'src/features/auth/ui/IconInputComponent.vue';
+import TitleComponent from 'src/features/auth/ui/TitleComponent.vue';
 import ButtonComponent from 'src/shared/ui/ButtonComponent.vue';
+import FullScreenForm from 'src/widgets/FullScreenForm.vue';
 </script>
-
-<style scoped lang="scss">
-.full-screen-form {
-  width: 100wh;
-  height: 100vh;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  background-color: var(--color-background-secondary);
-
-  &__content {
-    width: 90vw;
-    max-width: var(--width-full-screen-form);
-    padding: var(--gap-usual);
-    background-color: var(--color-background-main);
-    border-top: var(--border-medium) solid var(--color-action);
-    display: flex;
-    flex-direction: column;
-    border-radius: var(--border-radius-secondary);
-  }
-
-  &__row {
-    margin-bottom: var(--gap-smaller);
-
-    &:last-child {
-      margin-bottom: 0;
-    }
-  }
-
-  &__small-button {
-    align-self: flex-end;
-    margin-bottom: var(--gap-smaller);
-  }
-}
-</style>
---
+++ front/src/widgets/FullScreenForm.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="full-screen-form">
+    <div class="full-screen-form__content">
+      <slot />
+    </div>
+  </div>
+</template>
